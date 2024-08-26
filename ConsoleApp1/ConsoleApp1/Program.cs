dabb3b5df6dd86fdbac252a58488e760a357771b
Laboratorio C205 <labc205@uam.net.ni>
2024-08-26 08:57:32 -0600

    All

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using Sesion2.Clases;
+
+namespace ConsoleApp1
+{
+    //Leer los datos de una persona y decir si es mayor o menor de edad
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string nombre;
+            DateTime fechaNac;
+            Persona per=new Persona();
+
+            Console.Write("Dime tu nombre: ");
+            nombre= Console.ReadLine();
+            Console.Write("En que fecha naciste: ");
+            fechaNac = DateTime.Parse(Console.ReadLine());
+
+            per.Nombre = nombre;
+            per.FechaNac = fechaNac;
+            Console.WriteLine(per.EvaluarEdad());
+        }
+    }
+}
