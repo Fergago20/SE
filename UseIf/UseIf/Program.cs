2575cb748e022d9daa43d010ebc4cf60143cc30e
Laboratorio C205 <labc205@uam.net.ni>
2024-09-02 08:51:19 -0600

    all

---
+++ UseIf/UseIf/Program.cs
@@ -0,0 +1,25 @@
+﻿using UseIf.models;
+
+namespace UseIf
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Decir si es nino, joven adulto
+            int edad = 0;
+            Evaluate evaluate = new Evaluate();
+
+            Console.WriteLine("Evaluar edad");
+            Console.WriteLine("Ingrese su edad");
+            try
+            {
+                edad = int.Parse(Console.ReadLine());
+                Console.WriteLine(evaluate.EvalAge(edad));
+            }
+            catch (Exception ex) { Console.WriteLine(ex.ToString()); }
+            
+           
+        }
+    }
+}
---
+++ UseIf/UseIf/models/Evaluate.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UseIf.models
+{
+    internal class Evaluate
+    {
+        public Evaluate() { }
+        public string EvalAge(int age)
+        {
+            try
+            {
+
+
+                if (age >= 0 && age <= 12) { return "Eres ninno"; }
+                else if (age >= 13 && age <= 25) { return "Eres Joven"; }
+                else if (age >= 26 && age<=120) { return "Eres Adulto"; }
+            }
+            catch (Exception ex)
+            {
+                return $"Error {ex.ToString}";
+            }
+            return "Edad no admitida";
+
+        }
+    }
+}
